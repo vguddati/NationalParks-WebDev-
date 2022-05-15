0f1ad6b8887a2c9f3e19a441766edb25522531fd
vguddati <77364999+vguddati@users.noreply.github.com>
2022-05-14 22:54:31 -0400

    Add files via upload

---
+++ Models/AppModel.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DIS_Group10.Models
+{
+    public class Park
+    {
+        public string ID { get; set; }
+        public string url { get; set; }
+        public string fullName { get; set; }
+        public string parkCode { get; set; }
+        public string description { get; set; }
+        public ICollection<StatePark> states { get; set; }
+        public ICollection<ParkActivity> activities { get; set; }
+    }
+
+    public class Activity
+    {
+        public string ID { get; set; }
+        public string name { get; set; }
+        public ICollection<ParkActivity> parks { get; set; }
+    }
+
+    public class ParkActivity
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int ID { get; set; }
+        public Activity activity { get; set; }
+        public Park park { get; set; }
+    }
+
+    public class State
+    {
+        public string ID { get; set; }
+        public string name { get; set; }
+        public ICollection<StatePark> parks { get; set; }
+    }
+
+    public class StatePark
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int ID { get; set; }
+        public State state { get; set; }
+        public Park park { get; set; }
+    }
+
+    public class UpdatePark
+    {
+        [Key]
+        public string ID { get; set; }
+        [Required]
+        [Url]
+        public string url { get; set; }
+        [Required]
+        public string fullName { get; set; }
+        [Required]
+        public string parkCode { get; set; }
+        [Required]
+        public string description { get; set; }
+        [Required]
+        public ICollection<string> statenames { get; set; }
+        [Required]
+        public ICollection<string> activitynames { get; set; }
+    }
+
+    public class AddNewPark
+    {
+        public AddNewPark()
+        {
+            ID = new Guid();
+        }
+
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Key]
+        public Guid ID { get; set; }
+        [Required]
+        [Url]
+        public string url { get; set; }
+        [Required]
+        public string fullName { get; set; }
+        [Required]
+        public string parkCode { get; set; }
+        [Required]
+        public string description { get; set; }
+        [Required]
+        public ICollection<string> statenames { get; set; }
+        [Required]
+        public ICollection<string> activitynames { get; set; }
+    }
+}
